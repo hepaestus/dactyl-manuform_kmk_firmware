c0e3cd73e191d12dd3dffa972c9655ff6c891162
Pete Olsen III <hepaestus@gmail.com>
2022-06-10 16:41:47 -0400

    Latest updates Version 1.0.13.rgb

--- boards/dactyl-manuform/6x6/main.py
+++ boards/dactyl-manuform/6x6/main.py
@@ -1,13 +1,14 @@
 ##################################################################
 author='Pete Olsen III hepaestus@gmail.com https://hepaestus.com/'
 manufacturer='Olsen Design 06-2022'
-version = 'Version 1.0.11'
+version = 'Version 1.0.13.rgb'
 ##################################################################
 print("Starting...")
 
 import board
 import neopixel # Adafruit NeoPixels Lib
 import adafruit_pioasm
+# import key_names
 from storage import getmount
 from kmk.keys import KC
 from kmk.modules.encoder import EncoderHandler
@@ -21,13 +22,13 @@ from kmk.scanners.digitalio import MatrixScanner
 
 ## Wiring/Build Specific Configuration ##########################################
 #
-rgb_pixel_pin = board.D2
-split_data_pin = board.D3
+my_rgb_pixel_pin = board.D2
+my_split_data_pin = board.D3
 my_col_pins=[board.A2, board.A1, board.A0, board.SCK, board.MISO, board.MOSI, board.D10]
 my_row_pins=[board.D4, board.D5, board.D6, board.D7, board.D8, board.D9]
 my_diode_orientation=DiodeOrientation.COL2ROW
 my_split_side=SplitSide.LEFT # DEFAULT TO LEFT SIDE
-neopixels_per_side = 24
+neopixels_per_side=24
 debugging_on=True
 #
 ## End Build Config #############################################################
@@ -46,20 +47,20 @@ else:
 
 ### Extensions
 rgb_ext = RGB(
-    pixel_pin=rgb_pixel_pin,
-    num_pixels=neopixels_per_side,
-    val_limit=100,
-    hue_default=100,
-    sat_default=100,
+    pixel_pin=my_rgb_pixel_pin,
+    num_pixels=neopixels_per_side*2,
+    val_limit=128,
+    hue_default=126,
+    sat_default=255,
     rgb_order=(1, 0, 2),  # GRB WS2812
-    val_default=50,
-    hue_step=5,
-    sat_step=5,
-    val_step=5,
-    animation_speed=1,
-    breathe_center=1,  # 1.0-2.7
-    knight_effect_length=3,
-    animation_mode=AnimationModes.STATIC,
+    val_default=64,
+    hue_step=16,
+    sat_step=16,
+    val_step=16,
+    animation_speed=3,
+    breathe_center=2,  # 1.0-2.7
+    knight_effect_length=4,
+    # animation_mode=AnimationModes.STATIC,
     reverse_animation=False,
     refresh_rate=60
 )
@@ -70,7 +71,7 @@ split_mod = Split(
     split_flip=True, # If both halves are the same, but flipped, set this True
     split_type=SplitType.UART,  # Defaults to UART
     uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
-    data_pin=split_data_pin,  # The primary data pin to talk to the secondary device
+    data_pin=my_split_data_pin,  # The primary data pin to talk to the secondary device
     #data_pin2=split_data_pin,  # Second uart pin to allow 2 way communication
     use_pio=True,  # allows for UART to be used with PIO
 )
@@ -123,21 +124,23 @@ keyboard.debug_enabled = debugging_on
 # keyboard.row_pins=my_row_pins
 # keyboard.col_pins=my_col_pins
 
+keyboard.pixel_pin=my_rgb_pixel_pin
+keyboard.num_pixels=neopixels_per_side*2
+
 keyboard.modules.append(split_mod)
 keyboard.modules.append(Layers())
 keyboard.extensions.append(rgb_ext)
 
 keyboard.coord_mapping = [
-     0, 1,  2,  3,   4,  5,    48, 47, 46, 45, 44, 43,
-     6, 7,  8,  9,  10, 11,    54, 53, 52, 51, 50, 49,
-    12, 13, 14, 15, 16, 17,    60, 59, 58, 57, 56, 55,
-    18, 19, 20, 21, 22, 23,    66, 65, 64, 63, 62, 61,
-    24, 25, 26, 27, 28, 29,    72, 71, 70, 69, 68, 67,
-            32, 33, 34, 35,    78, 77, 76, 75,
-            39, 40, 41, 42,    84, 83, 82, 81,
+     0, 1,  2,  3,   4,  5,    47, 46, 45, 44, 43, 42,
+     6, 7,  8,  9,  10, 11,    53, 52, 51, 50, 49, 48,
+    12, 13, 14, 15, 16, 17,    59, 58, 57, 56, 55, 54,
+    18, 19, 20, 21, 22, 23,    65, 64, 63, 62, 61, 60,
+    24, 25, 26, 27, 28, 29,    71, 70, 69, 68, 67, 66,
+    30, 31, 32, 33, 34, 35,    77, 76, 75, 74, 73, 72,
+            38, 39, 40, 41,    83, 82, 81, 80,
 ]                                          
 
-# Cleaner key names
 XXXXXXX = KC.NO
 _______ = KC.TRNS
 UNDO = KC.LCTL(KC.Z)
@@ -151,38 +154,45 @@ NEXT = KC.LALT(KC.RGHT)
 LBSPC = KC.LCTL(KC.BSPC)
 LOWER = KC.MO(1)
 RAISE = KC.MO(2)
+RGBSWRL = KC.RGB_MODE_SWIRL
+RGBTOG = KC.RGB_TOG
+RGBBRTH = KC.RGB_MODE_BREATHE
+RGBBOW = KC.RGB_MODE_RAINBOW
+RGBBBW = KC.RGB_MODE_BREATHE_RAINBOW
+RGBKNT = KC.RGB_MODE_KNIGHT
+
 
 keyboard.keymap = [
     # QWERTY
     [    
-        KC.F1,  KC.F2,  KC.F3,  KC.F4,  KC.F5,  KC.F6,  KC.F7,  KC.F8,  KC.F9,  KC.F10, KC.F11, KC.F12,
-        KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,  KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,
-        KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.EQL,
-        KC.LSFT,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.RSFT,
-        KC.LCTL,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.BSLASH,
-                        KC.LCBR,KC.RCBR,KC.ENT, KC.SPC, KC.SPC, KC.RGUI,KC.HOME,KC.END, 
-                        LOWER,  CUT    ,KC.TAB, KC.BSPC,KC.DEL, PASTE  ,KC.DEL, RAISE,
+        KC.F1,  KC.F2,  KC.F3,  KC.F4,  KC.F5,  KC.F6,       KC.F7,  KC.F8,  KC.F9,  KC.F10, KC.F11, KC.F12,
+        KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,       KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,
+        KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,        KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.EQL,
+        KC.LSFT,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,        KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.RSFT,
+        KC.LCTL,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,        KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.BSLASH,
+        KC.LCBR,KC.RCBR,KC.ENT, KC.SPC, KC.SPC, LOWER,       RAISE,  KC.BSPC,KC.DEL, PASTE,
+                        KC.BSPC,RGBSWRL,RGBTOG, KC.DEL,      KC.DEL, RGBBRTH,RGBKNT, RGBBOW,
     ],
     # LOWER
-    [       
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC.PGUP, KC.UP,   KC.PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC.LEFT, KC.DOWN, KC.RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          _______, _______, _______, _______, _______, _______, _______, _______,
-                          _______, _______, _______, _______, _______, _______, _______, _______,
-    ],
+    [           
+        _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC.PGUP, KC.UP,   KC.PGDN, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC.LEFT, KC.DOWN, KC.RGHT, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, RGBSWRL, RGBBBW,  RGBBOW ,KC.RGB_HUD,_______,   _______,KC.RGB_HUI,RGBTOG, RGBKNT , RGBBRTH, RGBKNT, 
+                          _______, _______, _______, _______,    _______, _______, _______, _______,
+    ],    
     #RAISE
     [       
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.PSCR, KC.MUTE, KC.VOLU, KC.VOLD,
-        _______, XXXXXXX, KC.PGUP, KC.UP,   KC.PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC.LEFT, KC.DOWN, KC.RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          _______, _______, _______, _______, _______, _______, _______, _______,
-                          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, KC.PSCR, KC.MUTE, KC.VOLU, KC.VOLD,
+        _______, XXXXXXX, KC.PGUP, KC.UP,   KC.PGDN, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC.LEFT, KC.DOWN, KC.RGHT, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, 
+                          _______, _______, _______, _______,   _______, _______, _______, _______,
     ],
 ]
 
 print("Started! Version: {}".format(version))
-if __name__ == '__main__':    
+if __name__ == '__  main__':
     keyboard.go()
